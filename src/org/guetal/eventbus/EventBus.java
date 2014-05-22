fa41b14bfbb18a84be0690e4552abdbc72a07ce8
DevGuetal <dev.guetal@gmail.com>
2014-05-22 13:48:23 +0100

    EventBus
    A simple event bus, written initially for Android, but works everywhere.
    Provide a simple mechanism to register to events and broadcast them. No
    argument passing is possible.. yet..

---
+++ src/org/guetal/eventbus/EventBus.java
@@ -0,0 +1,62 @@
+package org.guetal.eventbus;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+public class EventBus {
+	
+	private static final String TAG = EventBus.class.getName();
+
+	private static EventBus instance = null;
+	
+	private HashMap<String, HashMap<Object, String>> eventMap = null;
+	
+	public static EventBus getInstance() {
+		if(instance == null) {
+			instance = new EventBus();
+		}
+		return instance;
+	}
+	
+	private EventBus() {
+		this.eventMap = new HashMap<String, HashMap<Object,String>>();
+	}
+	
+	public void subscribeEvent(String eventName, String function, Object className) {
+		HashMap<Object, String> eventCallbacks = this.eventMap.get(eventName);
+		if(eventCallbacks == null) {
+			eventCallbacks = new HashMap<Object, String>();
+			this.eventMap.put(eventName, eventCallbacks);
+		}
+		
+		if(!(eventCallbacks.containsKey(className) && eventCallbacks.get(className).equals(function))) { 
+			eventCallbacks.put(className, function);
+		}
+	}
+	
+	public void broadcastEvent(String eventName) {
+		HashMap<Object, String> eventCallbacks = this.eventMap.get(eventName);
+		
+		Set<Object> keys = eventCallbacks.keySet();
+		Iterator<Object> it = keys.iterator();
+		
+		while (it.hasNext()){
+			Object objReference = it.next();
+			String callback = eventCallbacks.get(objReference);
+			try {
+
+				Class<?> c = Class.forName(objReference.getClass().getName());
+				Method  method = c.getDeclaredMethod (callback);
+				method.invoke (objReference);
+			} catch (Exception e) {
+				//Log.e(TAG, "Error while processing callback on event. EventName="+eventName+". Class="+objReference.getClass().getName()+". Error="+e.getMessage(), e);
+				//Log some error here
+			}
+		}
+		
+		
+	}
+	
+}
